a5bf767ca2d130cd8806641f4984ef98768b420a
Leongu <2653579853@qq.com>
2025-11-24 20:42:55 -0500

    Added character running animation

---
+++ Source/ResearchProject/Private/Anim/PlayerAnim.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Anim/PlayerAnim.h"
+#include "player/PlayerCharacter.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UPlayerAnim::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	PlayerCharacter = Cast<APlayerCharacter>(TryGetPawnOwner());
+
+	if (PlayerCharacter) 
+	{
+		PlayerCharacterMovement = PlayerCharacter->GetCharacterMovement();
+	}
+
+}
+
+void UPlayerAnim::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (PlayerCharacterMovement)
+	{
+		speed = UKismetMathLibrary::VSizeXY(PlayerCharacterMovement->Velocity);
+
+	}
+}
